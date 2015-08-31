df6eaef20f83fb2cb43e63461185a180cb1742cc
pmgreen <pmcgreen@gmail.com>
2015-08-31 16:57:09 -0400

    from shelve to sqlite, generate simple d3plus report

--- check.py
+++ check.py
@@ -19,46 +19,32 @@ import csv
 import cx_Oracle
 import glob
 import os
-import pickle
 import requests
-import shelve
 import shutil
+import sqlite3 as lite
 import sys
 import time
 
-today = time.strftime('%Y%m%d')
-justnow = time.strftime("%m/%d/%Y %I:%M:%S %p")
+today = time.strftime('%Y%m%d') # for csv name
+todaydb = time.strftime('%Y-%m-%d %H:%M:%S') # for db
+justnow = time.strftime("%Y-%m-%d %I:%M:%S %p") # for log
+
+numorecs = 1000 # number of records to check
 
 # parse configuration file check.cfg
 config = ConfigParser.RawConfigParser()
-config.read('check.cfg')
+config.read('./config/check.cfg')
 indir = config.get('env', 'indir')
 outdir = config.get('env', 'outdir')
 logdir = config.get('env', 'logdir')
 share = config.get('env','share')
-shelf_file = config.get('cache','shelf_file')
+db = config.get('db','sqlite')
 
 USER = config.get('vger', 'user')
 PASS = config.get('vger', 'pw')
 PORT = config.get('vger', 'port')
 SID = config.get('vger', 'sid')
 HOST = config.get('vger', 'ip')
-
-#===============================================================================
-# Checked class
-#===============================================================================
-class Checked(object):
-	def __init__(self):
-		self.value = ""
-		"""Link as a string"""
-		self.response = ""
-		""""HTTP response code (or exception)"""
-		self.redirect_url = ""
-		""""If redirected, the new URL"""
-		self.redirect_status = ""
-		""""If redirected, response from the new URL"""
-		self.check_date = ""
-		"""Today's date and time"""
 
 #===============================================================================
 # defs
@@ -68,6 +54,7 @@ def main(picklist):
 	make_report(picklist)
 	if copy_report == True:
 		mv_outfiles()
+	make_pie()
 	print('all done!')
 
 
@@ -84,9 +71,10 @@ def get_bibs(picklist):
 	query = """SELECT RECORD_ID 
 			FROM ELINK_INDEX 
 			WHERE RECORD_TYPE = 'B'
+			AND LINK NOT LIKE '%%hdl.handle.net/2027%%'
 			AND RECORD_ID > %s
-			AND ROWNUM <= 1000
-			ORDER BY record_id""" % lastbib
+			AND ROWNUM <= %s
+			ORDER BY record_id""" % (lastbib, numorecs)
 
 	dsn = cx_Oracle.makedsn(HOST,PORT,SID)
 	oradb = cx_Oracle.connect(USER,PASS,dsn)
@@ -97,7 +85,7 @@ def get_bibs(picklist):
 	rows.close()
 	oradb.close()
 	
-	with open(indir+'bibs.csv','wb+') as outfile:
+	with open(indir+picklist,'wb+') as outfile:
 		writer = csv.writer(outfile)
 		header = ['BIB_ID']
 		writer.writerow(header) 
@@ -114,7 +102,7 @@ def make_report(picklist):
 	Input is the csv picklist. Output is report with HTTP statuses added.
 	"""
 	try:
-		os.remove(outdir+picklist) # remove ourput from previous runs
+		os.remove(outdir+picklist) # remove output from previous runs
 	except OSError:
 		pass
 		
@@ -136,8 +124,13 @@ def query_elink_index(bibid):
 	Query the ELINK_INDEX table
 	"""	
 	thisbib = [] # temp list of URLs per bib id; the same URL can appear in a bib more than once; let's check just once
-	shelf = shelve.open(shelf_file, protocol=pickle.HIGHEST_PROTOCOL) # cache
 	
+	cached = False
+	con = lite.connect('./db/cache.db')
+	response = ''
+	redirect_url = ''
+	redirect_status = ''
+
 	dsn = cx_Oracle.makedsn(HOST,PORT,SID)
 	db = cx_Oracle.connect(USER,PASS,dsn)
 	
@@ -157,38 +150,55 @@ def query_elink_index(bibid):
 		bib = row[0]
 		ti = row[1]
 		url = row[2]
-		
-		link = Checked()
-		
+	
 		if url not in thisbib: # if not already checked just now, under the same bib id...
-			if ignore_cache==False: # if checking the cache...
-				u = shelf.get(url,None) # to avoid exceptions when the key doesn't exist
-				if u is not None and (u.response == 200 or u.redirect_status == 200) and today in u.check_date:
-					# if the URL's in the cache, status was 200, and it was already checked today, just copy values from cache
-					resp = shelf[url].response
-					redir = shelf[url].redirect_url
-					redirst = shelf[url].redirect_status
-					last_checked = shelf[url].check_date
+			if ignore_cache==False: # if indeed checking the cache...
+				
+				with con:
+					con.row_factory = lite.Row
+					cur = con.cursor()
+					cur.execute("SELECT * FROM bibs WHERE bib=?",(bib,))
+					rows = cur.fetchall()
+					if len(rows) == 0:
+						cached = False
+					else:
+						cached = True
+						for row in rows:
+							response = row['status']
+							redirect_status = row['redirect_status']
+							check_date = row['last_checked']
+					
+				# if the URL's in the cache, status was 200, and it was already checked today, just copy values from cache	
+				if cached == True and (response == 200 or redirect_status == 200) and check_date == todaydb:
+					resp = response
+					redir = redirect_url
+					redirst = redirect_status
+					last_checked = check_date
 				else:
-					resp,redir,redirst = get_reponse(url)
-					last_checked = time.strftime('%Y%m%d %I:%M:%S')
-			else:
+					resp,redir,redirst = get_reponse(url) # ping
+					last_checked = todaydb
+			else: # if ignoring cache
 				resp,redir,redirst = get_reponse(url)
-				last_checked = time.strftime('%Y%m%d %I:%M:%S')
+				last_checked = todaydb
 				
 			# put the link and responses in the cache (even if not reading cache on this run)
-			link.value = url
-			link.response = resp
-			link.redirect = redir
-			link.redirect_status = redirst
-			link.check_date = last_checked
-			shelf[url] = link
-
+			with con:
+				cur = con.cursor() 
+				if cached == False:
+					# insert new url into db
+					newurl = (bib, url, resp, redir, redirst, todaydb)
+					cur.executemany("INSERT INTO bibs VALUES(?, ?, ?, ?, ?,?)", (newurl,))
+				else:
+					# or, if it was in the cache from a previous run (just in case)
+					updateurl = (url,resp, redir, redirst, todaydb, bib)
+					cur.executemany("UPDATE bibs SET url=?, status=?, redirect=?, redirect_status=?, last_checked=? WHERE bib=?", (updateurl,))
+			
 			thisbib.append(url)
 						
 			newrow = [bib,ti,url,resp,redir,redirst,last_checked]
-					
-			print("%s, %s, %s, %s, %s, %s, %s" % (bib,ti,url,resp,redir,redirst,last_checked))
+			
+			if verbose:
+				print("%s, %s, %s, %s, %s, %s" % (bib,url,resp,redir,redirst,last_checked))
 			
 			with open(outdir+picklist,'ab+') as outfile:
 				if resp != 200 and redirst != 200: # just report out the problems
@@ -197,7 +207,9 @@ def query_elink_index(bibid):
 		
 	thisbib = []
 
-	c.close()
+	if con:
+		con.close() # sqlite
+	c.close() # oracle
 
 def get_reponse(url):
 	"""
@@ -257,19 +269,90 @@ def mv_outfiles():
 		except:
 			print("problem with moving files: %s" % sys.exc_info()[1])
 			pass
-           
+  
+def make_pie():
+	"""
+	Generate simple pie chart
+	"""
+	htmlfile = open('./html/elink.html','wb+')
+	con = lite.connect('./db/cache.db')
+	with con:
+		con.row_factory = lite.Row
+		cur = con.cursor()
+		cur.execute("select count(bib) from bibs")
+		rows = cur.fetchall()
+		for row in rows:
+			total = str(row[0])
+	
+	header = """<!doctype html>
+<meta charset="utf-8">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<script src="http://www.d3plus.org/js/d3.js"></script>
+<script src="http://www.d3plus.org/js/d3plus.js"></script>
+<div class="container">
+<h1>Voyager Links</h1>
+<p>Start date: 08/31/2015</p>
+<p>Last report: """+time.strftime('%m/%d/%Y')+"""</p>
+<p>Statuses of the <span style='font-size:1.25em'>"""+total+"""</span> records checked so far...</p>
+<sub><a href='http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html'>status codes</a></sub>
+</div>
+<div id="viz" style="width:60%;height:600px;"></div>
+<script>
+
+ var attributes = [
+    {"name": "200", "hex": "#B2F0B2"}
+  ]
+
+  var data = ["""
+
+   
+	footer = """]
+  d3plus.viz()
+    .container("#viz")
+    .data(data)
+    .type("pie")
+    .color("heatmap")
+    .id("name")
+    .size("value")
+    .draw()
+
+</script> 
+"""
+	htmlfile.write(header)
+	
+	rownum = 0
+	with con:
+		con.row_factory = lite.Row
+		cur = con.cursor()
+		cur.execute("select status, count(status) from bibs group by status")
+		rows = cur.fetchall()
+		for row in rows:
+			response = row[0]
+			count = row[1]
+			if rownum == 1:
+				htmlfile.write('{"value":%s,"name":"%s"}' % (count,response))
+			elif rownum > 1:
+				htmlfile.write(',\n{"value":%s,"name":"%s"}' % (count,response))
+			rownum += 1
+	htmlfile.write(footer)
+	con.close()
+   
 if __name__ == "__main__": 
 	parser = argparse.ArgumentParser(description='Check ELINK_INDEX tables against list of BIB_IDs')
-	parser.add_argument('-f','--filename',required=False,type=str,dest="picklist",help="Optional. The name of picklist file, e.g. 'bibs_20150415.csv' (assumed to be in ./in). Can just be a list of BIB_IDs.")
+	parser.add_argument('-f','--filename',required=False,dest="picklist",help="Optional. The name of picklist file, e.g. 'bibs_20150415.csv' (assumed to be in ./in). Can just be a list of BIB_IDs.")
 	parser.add_argument("-C", "--ignore-cache",required=False, dest="ignore_cache", action="store_true", help="Optionally ignore the cache to test all URLs freshly.")
 	parser.add_argument("-c", "--copy-report",required=False, default=True, dest="copy_report", action="store_false", help="Do not copy the resulting report to the share specified in cfg file.")
+	parser.add_argument("-v", "--verbose",required=False, default=True, dest="verbose", action="store_true", help="Print out bibs and urls as it runs.")
 	args = vars(parser.parse_args())
 	picklist = args['picklist'] # the list of bibs
 	ignore_cache = args['ignore_cache']
 	copy_report = args['copy_report']
+	verbose = args['verbose']
 	
-	if not picklist:
+	if not picklist: # if no file given...
 		picklist = 'bibs_'+today+'.csv'
-		get_bibs(picklist)	
-	
+		get_bibs(picklist)
+
 	main(picklist)
+	
